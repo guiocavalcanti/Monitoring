588cba8d744fcbce22187f6f52d7539bbe27cf06
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-03-23 10:32:10 -0300

    Initial commit

---
+++ monitor.rb
@@ -0,0 +1,21 @@
+require 'logger'
+
+@global_opts = {
+  :log => 'monitor.log',
+  :results => 'results',
+  :default_host => 'www.google.com.br'
+}
+
+def logger(opts = {})
+  opts = @global_opts.merge(opts)
+  log = Logger.new(opts[:log])
+end
+
+def ping(host = nil)
+  host ||= @global_opts[:default_host]
+  result = `ping -c 5 #{host}`
+end
+
+logger.debug do
+  "\n" + ping + "\n" + ping("www.yahoo.com.br")
+end
